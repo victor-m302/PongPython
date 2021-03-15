3a4960f72261072901b7b490d3a836d4e5e77601
VictorM302 <victor-m302@users.noreply.github.com>
2021-03-14 22:00:47 -0300

    comentários

--- engine.py
+++ engine.py
@@ -1,16 +1,22 @@
 import pygame
 import random
 import time
+'''
+INICIALIZA PYGAMES
+'''
+pygame.init() 
 
-pygame.init()
+#DEFINE ALTURA, LAGURA
 width, height = 400, 400
 game_screen = pygame.display.set_mode((width, height))
+#CRIA O TÍTULO DA JANELA
 pygame.display.set_caption("Game Pong")
 
+#LOOP PRINCIPAL
 while True:
     events = pygame.event.get()
     for event in events:
-        if(event.type == pygame.QUIT):
+        if(event.type == pygame.QUIT): #SAI DO JOGO SE CLICAR NO X DE FECHAR
             pygame.quit()
             quit()
 
